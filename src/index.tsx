11f03e277e53ccbfcc008c277f1dd28a67b861a5
raitasu <pavelburyak3@gmail.com>
2022-08-03 16:33:12 +0300

    feat: created theme.ts and wrapped app by using chakra provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
+import { theme } from './shared/ui/theme/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +17,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
