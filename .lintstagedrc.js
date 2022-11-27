cfbce72a274e219ff6e7ef2b35e04c7aa6fa47cc
Stanislav <stanislav.evstifeev@cybergizer.com>
2022-11-27 18:18:16 +0300

    fix: add app loader

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{json,css,scss,md}': ['prettier --write'],
+  '*.{json,css,scss,md,html}': ['prettier --write'],
   '*.{js,jsx,ts,tsx}': ['eslint --fix'],
   '*.{ts,tsx}': () => 'tsc --noEmit --project tsconfig.json',
   'package.json': () =>
