548a87d3ef2d1f4836f21ff9961715d2e986d01b
Tania <t.sysueva031291@gmail.com>
2022-12-19 13:56:39 +1000

    feat:  make icon discord working (copied by click)

--- src/features/employees/Tables/cells/SocialNetworksCell.tsx
+++ src/features/employees/Tables/cells/SocialNetworksCell.tsx
@@ -1,8 +1,9 @@
 import { type FunctionComponent, type SVGProps } from 'react';
 
-import { Flex, Link } from '@chakra-ui/react';
+import { Flex, Link, useClipboard } from '@chakra-ui/react';
 import { type CellContext } from '@tanstack/react-table';
 
+import { IconButton } from '~/shared/ui/components/IconButton';
 import {
   type ShortEmployee,
   type SocialNetwork
@@ -29,10 +30,21 @@ const SocialNetworkIcons: Record<
   discord: DiscordIcon
 };
 
+const styles = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  boxSize: '24px',
+  color: 'brand.darkGray',
+  _hover: { color: 'brand.lightGray' },
+  _active: { color: 'brand.ghostGray' }
+};
+
 export const SocialNetworksCell = ({
   getValue
 }: CellContext<ShortEmployee, ShortEmployee['social_networks']>) => {
   const employeeNetworks = getValue();
+  const { onCopy, setValue } = useClipboard('');
 
   return (
     <Flex gap="10px">
@@ -44,24 +56,33 @@ export const SocialNetworksCell = ({
           ).map((network) => {
             const NetworkIcon: FunctionComponent<SVGProps<SVGSVGElement>> =
               SocialNetworkIcons[network];
+
             const link: string = employeeNetworks[network];
 
             if (!link) {
               return null;
             }
 
-            return (
+            const isDiscord = /\w+#\d{4}/i.test(link);
+
+            return isDiscord ? (
+              <IconButton
+                key={network}
+                icon={<NetworkIcon />}
+                aria-label={network}
+                onClick={() => {
+                  setValue(link);
+                  onCopy();
+                }}
+                sx={styles}
+                variant="iconButtonSmall"
+              />
+            ) : (
               <Link
                 key={network}
                 href={link}
                 target="_blank"
-                display="flex"
-                alignItems="center"
-                justify-content="center"
-                boxSize="24px"
-                color="brand.ghostGray"
-                _hover={{ color: 'brand.lightGray' }}
-                _active={{ color: 'brand.ghostGray' }}
+                sx={styles}
               >
                 <NetworkIcon />
               </Link>
